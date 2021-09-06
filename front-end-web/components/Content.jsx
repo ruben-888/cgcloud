8944997068e6d452e29248a54402de73063e9b49
Ruben Perez <ruben@kleos.com>
2021-09-06 13:53:00 -0700

    test push in other folder

--- front-end-web/components/Content.jsx
+++ front-end-web/components/Content.jsx
@@ -7,7 +7,7 @@ import contentData from '../utils/contentData';
 const Content = () => (
   <div className="next-steps my-5" data-testid="content">
     <h2 className="my-5 text-center" data-testid="content-title">
-      What can I do next?
+      What can I do next??
     </h2>
     <Row className="d-flex justify-content-between" data-testid="content-items">
       {contentData.map((col, i) => (
